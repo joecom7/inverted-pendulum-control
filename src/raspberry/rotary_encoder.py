9b188488d69a1334f3eb90099d19ac5d8297efa4
giovanni <giovanni.francesco11c@gmail.com>
2023-02-26 15:21:55 +0100

    Normalizzato l'angolo fra -pi e pi

--- src/raspberry/rotary_encoder.py
+++ src/raspberry/rotary_encoder.py
@@ -2,6 +2,7 @@
 
 import pigpio
 from math import pi
+import math
 
 class decoder:
 
@@ -17,7 +18,6 @@ class decoder:
       self.levB = 0
 
       self.lastGpio = None
-      self.pos = 0
 
       self.pi.set_mode(gpioA, pigpio.INPUT)
       self.pi.set_mode(gpioB, pigpio.INPUT)
@@ -29,11 +29,17 @@ class decoder:
       self.cbB = self.pi.callback(gpioB, pigpio.EITHER_EDGE, self._pulse)
 
       self.PPR=3000
+      self.INITIAL_ANGLE = 0
+      #self.INITIAL_ANGLE = math.pi
+      self.pos = (self.INITIAL_ANGLE/(2*math.pi))*self.PPR
 
    def callback(self,way):
 
       self.pos += way
-
+      if self.pos > self.PPR/2:
+         self.pos -= self.PPR
+      elif self.pos <= -self.PPR/2:
+         self.pos += self.PPR
    def _pulse(self, gpio, level, tick):
 
       """
