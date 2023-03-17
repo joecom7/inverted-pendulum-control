d17e4b446e260d7e36fe7cd60022f9f550a88c93
Giovanni Francesco Comune <giovanni.francesco11c@gmail.com>
2023-03-17 18:30:45 +0100

    Aggiunto output sulla distanza temporale fra i vari print

--- src/test.py
+++ src/test.py
@@ -1,9 +1,22 @@
+from math import sqrt
 import time
 import pigpio
 import rotary_encoder
+from datetime import datetime
 pi = pigpio.pi()
 decoder = rotary_encoder.decoder(pi, 17, 18)
 
+now = datetime.now()
+somma_delay = 0
+somma_quadratica_delay = 0
+iterazioni=0
+
 while True:
-    time.sleep(1)
-    print(decoder.get_angle_degrees())
+    time.sleep(921e-5)
+    iterazioni += 1
+    delay = (datetime.now()-now).total_seconds() * 1000
+    somma_delay += delay
+    somma_quadratica_delay += delay**2
+    media = somma_delay/iterazioni
+    print(str(decoder.get_angle())+","+str(media)+","+str(sqrt((somma_quadratica_delay / iterazioni) - (media)**2 )))
+    now = datetime.now()
