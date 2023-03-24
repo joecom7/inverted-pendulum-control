035860cda460c6b2a5bf752861c77f4c5b57add2
Giovanni Francesco Comune <giovanni.francesco11c@gmail.com>
2023-03-24 10:13:09 +0100

    Corretto valore errato della costante di guadagno del feedback temporale

--- Constants.cpp
+++ Constants.cpp
@@ -5,5 +5,5 @@ const uint8_t Constants::ENCODER_CLK_PIN = 17;
 const uint8_t Constants::ENCODER_DT_PIN = 18;
 const uint16_t Constants::ENCODER_PPR = 1024;
 const uint8_t Constants::ENCODER_START_ANGLE_DEGREES = 0;
-const float Constants::DELAY_FEEDBACK_GAIN = 10;
+const float Constants::DELAY_FEEDBACK_GAIN = 1.2;
 const bool Constants::BYPASS_ROBOT = true;
